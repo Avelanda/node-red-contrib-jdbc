30cd7091bd08691edfa534e64b0748646c16e56e
Avelanda <gorthell@gmail.com>
2025-09-20 03:08:19 +0200

    Update connector.js
    Structural execution of modular  functional input.

--- connector.js
+++ connector.js
@@ -1,7 +1,26 @@
-const fs = require('fs');
-const path = require("path");
+/*
+ * Copyright © 2025 [Avelanda]
+ * All rights reserved 
+ */
 
-module.exports = function (RED) {
+ function State(){
+  
+  function CoreState(){
+   const fs = require('fs');
+   const path = require("path");
+
+   module.exports = function (RED) {
     const flow = fs.readFileSync(path.join(__dirname, "subflow.json"));
     RED.nodes.registerSubflow(JSON.parse(flow));
-}
+   }
+  }
+  
+  for (CoreState == null; CoreState != undefined; CoreState){
+      return CoreState();
+  }
+   if (CoreState == !false || !true){
+    return 0;
+   } else { return 1;}
+ 
+ }
+  State(); 
